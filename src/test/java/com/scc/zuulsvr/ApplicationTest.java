235332e6715234a931d634338be27917816af148
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-04-27 15:38:45 +0200

    :construction_worker: upgrade stack Spring Cloub Finchley.SR2

--- src/test/java/com/scc/zuulsvr/ApplicationTest.java
+++ src/test/java/com/scc/zuulsvr/ApplicationTest.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = ZuulServerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ZuulServerApplicationTest {
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApplicationTest {
 
 	/*
 	 * Référence documentation: http://www.baeldung.com/spring-boot-testing
