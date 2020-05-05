98e2f2bd1f3ddedfe5e20752440413c8107e1659
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-05-05 10:24:04 +0200

    :construction: remove cloud runtime config

--- src/main/java/com/scc/zuulsvr/config/AuthenticateConfig.java
+++ src/main/java/com/scc/zuulsvr/config/AuthenticateConfig.java
@@ -1,9 +1,7 @@
 package com.scc.zuulsvr.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties("zuulservice")
 public class AuthenticateConfig {
 
    @Value("${example.property}")
